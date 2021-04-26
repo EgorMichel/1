bf7ef6d3208fa3e8ddeaf289540e303cf3e99e1c
alryt <74343693+alryt@users.noreply.github.com>
2021-04-26 16:30:36 +0300

    Add files via upload
    частичка

--- text.py
+++ text.py
@@ -1,26 +1,34 @@
 import pygame
+import sys
 
 pygame.init()
 
-W = 600
-H = 400
-
-sc = pygame.display.set_mode((W, H))
-
-FPS = 60
-clock = pygame.time.Clock()
-counter = 0
-while 1:
+size = (510, 510)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Крестики-нолики")
+width = heigth = 40
+margin = 10
+red = (255, 0, 0)
+white = (255, 255, 255)
+mas = [[0]*10 for i in range(10)]
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit()
+            pygame.quit()
+            sys.exit(0)
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if (counter % 2 == 0):
-                print("Игрок А")
-                counter += 1
+            x_mouse, y_mouse = pygame.mouse.get_pos()
+            print(f'x={x_mouse} y={y_mouse}')
+            column = x_mouse//(margin + width)
+            row = y_mouse//(margin + heigth)
+            mas[row][column] = mas[row][column] ^ 1
+    for row in range(10):
+        for col in range(10):
+            if mas[row][col] == 1:
+                color = red
             else:
-                print("Игрок Б")
-                counter += 1
-
-    clock.tick(FPS)
-
+                color = white
+            x = col*width + (col+1)*margin
+            y = row*heigth + (row+1)*margin
+            pygame.draw.rect(screen, color, (x, y, width, heigth))
+    pygame.display.update()
